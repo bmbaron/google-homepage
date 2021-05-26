d1bed247ed39df8619492a0f448e27ca6d41f51a
Ben Baron <benjamin.m.baron@gmail.com>
2021-05-26 09:50:23 +0700

    Made a functioning google homepage with search and I'm feeling lucky buttons. Added CSS and JS

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("button1");
+
+x.onclick = function(){
+    var search = document.getElementById('searchbox').value;
+    window.open("http://www.google.com/"+"search?q="+search);
+};
+
+var y = document.getElementById("button2");
+
+y.onclick = function(){
+    var search = document.getElementById('searchbox').value;
+    window.open("http://www.google.com/"+"search?q="+search+"&btnI=1");    
+
+};
